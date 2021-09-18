6057311fa573e838f467466655342ce1d99a34aa
Olivier DEBON <olivier@debon.net>
2021-09-18 08:45:08 +0200

    New font and optimization

---
+++ DmTft/Inc/font-8x16.h
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2005 Martin Decky
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ * - Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the distribution.
+ * - The name of the author may not be used to endorse or promote products
+ *   derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/** @addtogroup genarch
+ * @{
+ */
+/** @file
+ */
+
+#ifndef FONT_8X16_H_
+#define FONT_8X16_H_
+
+// 32 thru 127  + 192 thru 255
+#define FONT_GLYPHS     ((127 - 32 + 1) + (255 - 192 + 1))
+
+#define LOWERSET_FIRST_CODE 32
+#define LOWERSET_LAST_CODE 127
+#define UPPERSET_FIRST_CODE 192
+#define UPPERSET_LAST_CODE 255
+
+#define FONT_CHAR_WIDTH    8
+#define FONT_CHAR_HEIGHT  16
+
+extern const unsigned char fb_font[FONT_GLYPHS*16];
+
+int getGlyphIndex(unsigned char c);
+
+#endif
+
+/** @}
+ */
